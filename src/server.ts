d9990d42d478fd8c0e7092b2122bd6126e4588bc
asmdnayeeem <asmdnayeem245@gmail.com>
2025-05-28 13:27:03 +0530

    / route added

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,15 @@ dotenv.config();
 import app from './app';
 
 const PORT = process.env.PORT || 5000;
-
+import { Response } from 'express';
+app.get("/",async (_,res:Response)=>{
+  res.status(200).json({
+    message: "Welcome to the Gadget Store API",
+    version: "1.0.0",
+    documentation: "/api-docs"
+  });
+  return
+})
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
